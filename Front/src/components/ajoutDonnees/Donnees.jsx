6bb886fae944702fef9faec3b8c883f359649166
KvmjfuufEvdspt <juliette.ducros2005@gmail.com>
2025-12-08 16:57:07 +0100

    test données

--- Front/src/components/ajoutDonnees/Donnees.jsx
+++ Front/src/components/ajoutDonnees/Donnees.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 function Donnees(){
     const [data, setData] = useState([]);
+    const [headers, setHeaders] = useState([]);
 
-
-    function previewXLSFile (e) {
+    function handleChange (e) {
         var file = e.target.files[0];
         let reader = new FileReader();
         reader.onload = function(e) {    
@@ -12,38 +12,47 @@ function Donnees(){
             
             const lines = text.split("\n").map(l => l.trim()).filter(l => l !== "");
 
-            const headers = lines[0].split(",");
-            console.log(headers); // à voir
+            const newHeaders = lines[0].split(",");
+            setHeaders(newHeaders);
 
-            const json = lines.slice(1).map(line => {
+            const rows = lines.slice(1).map(line => {
                 const values = line.split(",");
                 let obj = {};
-                headers.forEach((h, i) => {
+                newHeaders.forEach((h, i) => {
                     obj[h] = values[i];
                 });
                 return obj;
             });
 
-            console.log("JSON :", json); // à voir --> plutôt 1 tab js ?
-            setData(json);
+            console.log("tab :", rows);
+            setData(rows);
         }
         reader.readAsText(file);
     }
 
-
-    function handleChange(e){
-        previewXLSFile(e);
-
-        // console.log(headers);
-    }
 
     return(
         <div>
             <label htmlFor="csv" id="labelCsv">Source de données</label>
             <input type="file" name="csv" id='csv' accept=".csv" onChange={handleChange} required ></input>
         
-            <table>
-
+             <table border="1" cellPadding="5" cellSpacing="0">
+                <thead>
+                    <tr>
+                        {headers.map((header, index) => (
+                        <th key={index}>{header}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((row, index) => (
+                        <tr key={index}>
+                        {headers.map((header, idx) => (
+                            <td key={idx}>{row[header]}</td>
+                        ))}
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
     )
