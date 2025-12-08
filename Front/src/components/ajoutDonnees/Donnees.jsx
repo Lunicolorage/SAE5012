2bf7e90600abe729d5280ee7a71b189572911b62
KvmjfuufEvdspt <juliette.ducros2005@gmail.com>
2025-12-08 16:14:19 +0100

    premiers test données

--- Front/src/components/ajoutDonnees/Donnees.jsx
+++ Front/src/components/ajoutDonnees/Donnees.jsx
@@ -1,21 +1,50 @@
+import { useState } from "react";
 
 function Donnees(){
+    const [data, setData] = useState([]);
+
+
     function previewXLSFile (e) {
         var file = e.target.files[0];
         let reader = new FileReader();
         reader.onload = function(e) {    
-            let data = e.target.result;
-            console.log(data);
+            let text = e.target.result;
+            
+            const lines = text.split("\n").map(l => l.trim()).filter(l => l !== "");
+
+            const headers = lines[0].split(",");
+            console.log(headers); // à voir
+
+            const json = lines.slice(1).map(line => {
+                const values = line.split(",");
+                let obj = {};
+                headers.forEach((h, i) => {
+                    obj[h] = values[i];
+                });
+                return obj;
+            });
+
+            console.log("JSON :", json); // à voir --> plutôt 1 tab js ?
+            setData(json);
         }
-        reader.readAsArrayBuffer(file);
+        reader.readAsText(file);
     }
-    var inputFile = document.getElementById("csv");
-    inputFile.addEventListener("change", previewXLSFile, false);
+
+
+    function handleChange(e){
+        previewXLSFile(e);
+
+        // console.log(headers);
+    }
 
     return(
         <div>
-            <label for="csv" id="labelCsv">Source de données</label>
-            <input type="file" name="csv" id='csv' accept=".xls, .xlsx, .csv" required ></input>
+            <label htmlFor="csv" id="labelCsv">Source de données</label>
+            <input type="file" name="csv" id='csv' accept=".csv" onChange={handleChange} required ></input>
+        
+            <table>
+
+            </table>
         </div>
     )
 }
