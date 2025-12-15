8b7e896fe5c228c0c709df6d4914e0535e291ad5
lunicolor <lulukonami@gmail.com>
2025-12-15 11:10:14 +0100

    routes react

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,15 +3,32 @@ import './App.css'
 import { Donnees } from './components/ajoutDonnees/Donnees'
 import { Articles } from './components/index/Articles'
 import { PageCreationArticle } from './components/creationArticle/PageCreationArticle'
+import { Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { NotFound } from './components/NotFound';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
 
   return (
-    <div>
-      {/* <Donnees /> */}
-      {/* <Articles/> */}
-      <PageCreationArticle />
-    </div>
+    <>
+    < BrowserRouter >
+      <Header />
+
+      <Routes>
+
+          <Route path="/create" element={<PageCreationArticle />} />
+          <Route path="/add-data" element={<Donnees />} />
+          <Route path="/index" element={<Articles/>} />
+          {/* <Route path="/shop" element={<Shop like={[tabLiked, setTabLiked]} card={[tabPanier, setTabPanier]} />} />  */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+    </>
   )
 }
 
