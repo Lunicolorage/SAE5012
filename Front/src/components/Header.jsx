89c3771685537124c5d218fcb1522136ee42ca94
lunicolor <lulukonami@gmail.com>
2025-12-17 09:55:58 +0100

    header burger

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
 
 function Header(){
 
     const menuBurger = document.querySelector('.menuBurger');
+
+    const [burgerOuvert, setBurgerOuvert] = useState(false);
+
+    useEffect(() => {
+    if(menuBurger){
+        menuBurger.classList.toggle('hide');
+    }
+  }, [burgerOuvert]);
 
     return(
         <header>
@@ -25,11 +34,16 @@ function Header(){
             </Link>
             <img 
                 onClick={() => {
-                    menuBurger.classList.toggle('hide');
+                    setBurgerOuvert(!burgerOuvert);
                 }}
-            src="src/assets/burger.svg" alt="icone menu burger" className="hideOrdi" />
+                src="src/assets/burger.svg" alt="icone menu burger" className="hideOrdi" />
 
             <section className="menuBurger hide">
+                <img
+                    onClick={() => {
+                        setBurgerOuvert(!burgerOuvert);
+                    }}
+                    src="src/assets/croix.png" alt="icone fermeture burger" />
                 <Link to="/index">
                 Index
                 </Link>
