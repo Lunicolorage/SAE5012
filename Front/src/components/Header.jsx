f27fc866a4ac6f3446654180ca07917de39f7f03
lunicolor <lulukonami@gmail.com>
2025-12-17 09:14:35 +0100

    var couleur

--- Front/src/components/Header.jsx
+++ Front/src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 function Header(){
+
+    const menuBurger = document.querySelector('.menuBurger');
+
     return(
         <header>
             <div className="header-left">
@@ -18,9 +21,28 @@ function Header(){
                 </Link>
             </div>
             <Link to="/connexion" className="hideMobile" >
-                Connection
+                Connexion
             </Link>
-            <img src="src/assets/burger.svg" alt="icone menu burger" className="hideOrdi" />
+            <img 
+                onClick={() => {
+                    menuBurger.classList.toggle('hide');
+                }}
+            src="src/assets/burger.svg" alt="icone menu burger" className="hideOrdi" />
+
+            <section className="menuBurger hide">
+                <Link to="/index">
+                Index
+                </Link>
+                <Link to="/create">
+                Ajout d'article
+                </Link>
+                <Link to="/add-data">
+                Ajout de données
+                </Link>
+                <Link to="/connexion">
+                    Connexion
+                </Link>
+            </section>
         </header>
     );
 }
