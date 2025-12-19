ef5477ddf377b32d419651568ddef11724bb91f5
lunicolor <lulukonami@gmail.com>
2025-12-19 14:57:03 +0100

    ajout page d'accueil aframe

--- Front/src/components/accueil/Accueil.jsx
+++ Front/src/components/accueil/Accueil.jsx
@@ -1,9 +1,28 @@
-function Accueil(){
-    return(
-        <div>
-            <p>Accueil</p>
-        </div>
-    );
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Accueil() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      if (event.data.action === 'navigateToIndex') {
+        navigate('/index');
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, [navigate]);
+
+  return (
+    <div>
+      <iframe 
+        src="/quiz.html" 
+        style={{width: '100%', height: '100vh', border: 'none'}}
+      />
+    </div>
+  );
 }
 
-export {Accueil};
+export { Accueil };
