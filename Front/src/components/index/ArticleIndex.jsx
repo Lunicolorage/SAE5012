f23f9f3a3c1cda503aea951515180e789bcc9f34
lunicolor <lulukonami@gmail.com>
2026-01-20 11:32:15 +0100

    affichage bouton suppr autorisation

--- Front/src/components/index/ArticleIndex.jsx
+++ Front/src/components/index/ArticleIndex.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../context/UserProvider";
+
 function ArticleIndex({ article, onArticleClick }) {
+
+  const [user, setUser] = useContext(UserContext);
+  const SuppArticleAllowed = ['ROLE_ADMIN','ROLE_EDIT'];
+  const hasAnyRole = (allowed) => Array.isArray(user?.roles) && user.roles.some(r => allowed.includes(r));
+  //verifie que le role autorise la fonctionnalité
+  //hasAnyRole(['ROLE_AUTEUR'] user.id==article.id
+
+   const IsTheAuthor = article.user.split('/').pop() == user.id;
+  // console.log(IsTheAuthor);
+
   return (
     <div className="article">
-      <h2>{article.titre}</h2>
+        <h2>{article.titre}</h2>
       <p>{article.resume}</p>
-      <button 
-        className="buttonArticle"
-        onClick={() => onArticleClick(article.id)}
-      >
-        Voir l'article
-      </button>
+      <div className="indexVoirSupp">
+        <button 
+          className="buttonArticle"
+          onClick={() => onArticleClick(article.id)}
+        >
+          Voir l'article
+        </button>
+        {hasAnyRole(SuppArticleAllowed) || IsTheAuthor && (
+          <button className="IndexTitre-suppr">Supprimer</button>
+        )}
+      </div>
     </div>
   );
 }
